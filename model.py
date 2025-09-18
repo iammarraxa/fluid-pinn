5e093806fc06b18a64fd8146f3f282bd080dfd56
Ammar <ammar.raza32002@gmail.com>
2025-09-18 09:57:40 +0500

    modified model.py. tweaked the PINN class a bit. added sigmoid as the default func. aslo added the dlexiblity to choose 4 diff activation funcs. made the code a bit modernized.

--- model.py
+++ model.py
@@ -2,15 +2,25 @@ import torch
 import torch.nn as nn
 
 class PINN(nn.Module):
-    def __init__(self, layers):
-        super(PINN, self).__init__()
-        self.hidden_layers = nn.ModuleList()
-        for i in range(len(layers) - 2):
-            self.hidden_layers.append(nn.Linear(layers[i], layers[i+1]))
-        self.output_layer = nn.Linear(layers[-2], layers[-1])
-        self.activation = torch.tanh  # For hidden layers only
+    def __init__(self, layers, activation="sigmoid"):
+        super().__init__()
+        acts = {
+            "sigmoid": nn.Sigmoid(),
+            "tanh": nn.Tanh(),
+            "relu": nn.ReLU(),
+            "gelu": nn.GELU(),
+        }
+        self.act = acts[activation]
+        net = []
+        for i in range(len(layers)-1):
+            lin = nn.Linear(layers[i], layers[i+1])
+            # Xavier init tailored to sigmoid/tanh
+            nn.init.xavier_uniform_(lin.weight)
+            nn.init.zeros_(lin.bias)
+            net.append(lin)
+            if i < len(layers)-2:
+                net.append(self.act)
+        self.net = nn.Sequential(*net)
 
     def forward(self, x):
-        for layer in self.hidden_layers:
-            x = self.activation(layer(x))
-        return self.output_layer(x)  # No activation here
+        return self.net(x)
